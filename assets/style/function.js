82335b1ba4129cf3182bbfe2c1998ad273c42983
extreme67 <123672387+extreme67@users.noreply.github.com>
2023-05-14 19:29:37 +0300

    function
    all file

---
+++ assets/style/function.js
@@ -0,0 +1,40 @@
+//Задание 1
+
+function showMessage() {
+console.log('Я учу JavaScript!'); 
+}
+
+showMessage();
+
+//Задание 2
+
+let theNum = "0";
+
+let cats = ["foto1", "foto2", "foto3", "foto4"];
+
+function goToRight() {
+    theNum++;
+    if (theNum == cats.length) {
+        theNum = "0";
+    }
+    change_image.src = './assets/image/' + cats[theNum] + '.jpg'; 
+}
+
+function goToLeft() {
+    theNum--;
+    if (theNum == "-1") {
+        theNum = cats.length - 1;
+    }
+    change_image.src = './assets/image/' + cats[theNum] + '.jpg';
+}
+
+//Задание со ***
+
+const year = +prompt('Введите год');
+
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)) {
+alert('Год является високосным');
+} 
+else {
+alert('Год не високосный');
+}
